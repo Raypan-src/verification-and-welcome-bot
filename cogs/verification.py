aa5eedc2b6bb2da3bff893b341cd72e326e7fd22
Raypan-src <raypan973@gmail.com>
2026-04-24 19:43:55 +0500

    Added verify command

--- cogs/verification.py
+++ cogs/verification.py
@@ -11,17 +11,31 @@ class verify(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         role_unverified = discord.utils.get(member.guild.roles, name="Unverified")
-        channel_verification = discord.utils.get(member.guild.text_channels, name="verification")
 
         if role_unverified:
             await member.add_roles(role_unverified)
-        
-        if channel_verification:
-            await channel_verification.set_permissions(role_unverified, view_channel=True, send_messages=True)
 
         for channels in member.guild.text_channels:
-            if channels != channel_verification:
+            if channels.name == "verification":
+                await channels.set_permissions(role_unverified, view_channel=True, send_messages=True)
+            else:
                 await channels.set_permissions(role_unverified, view_channel=False)
+
+    @app_commands.command(name="verify", description="Verify yourself")
+    @app_commands.checks.bot_has_permissions(manage_roles=True)
+    async def member_verify(self, interaction: discord.Interaction, member: discord.Member):
+        role_unverified = discord.utils.get(member.guild.roles, name="Unverified")
+        role_verify = discord.utils.get(member.guild.roles, name="Verified")
+
+        if role_verify:
+            member.add_roles(role_verify)
+            member.remove_roles(role_unverified)
+        
+        for channels in member.guild.text_channels:
+            if channels.name == "verification":
+                await channels.set_permissions(role_verify, view_channel=False)
+            else:
+                await channels.set_permissions(role_verify, view_channel=True)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(verify(bot))
